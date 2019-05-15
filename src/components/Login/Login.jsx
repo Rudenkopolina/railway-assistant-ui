951bbfa48875267aecdda39c1ce1783e18315e69
Darya Losich <losich.daria@gmail.com>
2019-05-15 11:33:27 +0300

    post request added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Login.css';
+import axios from 'axios';
 
 class Login extends React.Component {
   state = {
@@ -11,12 +12,24 @@ class Login extends React.Component {
     this.setState({ [title]: event.target.value });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    return axios({
+      method: 'post',
+      headers: {'content-type': 'application/json', "Authorization":'Basic ' + new Buffer.from(this.state.login + ':' + this.state.password).toString('base64')},
+      baseURL: `http://172.16.6.253:1000/api/auth/`,
+    })
+    .then(res => {
+      console.log(res);
+  });
+}
+
   render() {
     return (
       <div className='flex'>
         <div className='login-container'>
           <p className='header-text'>Пожалуйста, выполните вход</p>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input
               placeholder='Введите логин'
               className='input'
