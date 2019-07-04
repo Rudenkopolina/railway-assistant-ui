0334a5cc41222259a621605238ae81aecd07803d
Darya Losich <losich.daria@gmail.com>
2019-07-04 15:20:59 +0300

    small css fixes

--- src/components/ConversationsTable/ConversationModal/index.js
+++ src/components/ConversationsTable/ConversationModal/index.js
@@ -8,17 +8,23 @@ class ConversationModal extends React.Component {
 
   renderContent = () => {
     return (
-        <div className='modal-wrapper'>
+        <div className='modal-wrapper-conversation'>
           <div className='header'>
-            <div className='session-title'>{this.props.conversation.session}</div>
+            {/* <div className='session-title'>{this.props.conversation.session}</div> */}
             <div className='session-dates'>{(moment(this.props.conversation.timestamp_start)).format('DD.MM.YYYY HH:mm:ss')} - {(moment(this.props.conversation.timestamp_end)).format('DD.MM.YYYY HH:mm:ss')}</div>
           </div>
           <div className='body'>
             {this.props.messages.map((message, index) => {
               return (
                 <div key={index}>
-                  <div className='line'><div className='message-conversation message-conversation-user'>{message.requestText}</div></div>
-                  <div className='line'><div className='message-conversation message-conversation-system'>{message.responseText}</div></div>
+                  <div className='line'>
+                    <div className='message-conversation message-conversation-user'>{message.requestText}</div>
+                    <div className='message-conversation-time time-user'>Пользователь, {moment(message.timestamp).format('HH:mm:ss')}</div>
+                  </div>
+                  <div className='line'>
+                    <div className='message-conversation message-conversation-system'>{message.responseText}</div>
+                    <div className='message-conversation-time time-system'>Система, {moment(message.timestamp).format('HH:mm:ss')}</div>
+                  </div>
                 </div>
               );
             })}
