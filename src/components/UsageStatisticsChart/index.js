916cc14b48472f6af65602189981c70a8c1592a4
Darya Losich <losich.daria@gmail.com>
2019-07-08 13:20:46 +0300

    chart performance improved

--- src/components/UsageStatisticsChart/index.js
+++ src/components/UsageStatisticsChart/index.js
@@ -1,39 +1,51 @@
-import React from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import './styles.css'
-import { Chart } from "react-charts";
+import './styles.css';
+import { Chart } from 'react-charts';
 
+function UsageStatisticsChart(props) {
+  let [statistic, getStatistic] = useState({
+    statistics: [{ date: '2019-01-01', records: 0 }]
+  });
 
-class UsageStatisticsChart extends React.Component {
-	render() {
-		const { stats } = this.props;
-		let data = [];
-		stats.statistics.forEach(record => {
-			data.push([record.date, parseInt(record.records)]);
-		});
-		return (
-			<Chart
-				className='usage-statistics-chart-content'
-				data={[
-					{
-						label: this.props.label,
-						data: data.reverse()
-					}
-				]}
-				axes={[
-					{ primary: true, type: "ordinal", position: "bottom"},
-					{ type: "linear", position: "left", scaleIntegersOnly: true,}
-				]}
-				tooltip
-			/>
-		);
-	}
+  useEffect(() => {
+    if (props.stats) {
+      getStatistic((statistic = props.stats));
+    }
+  });
+
+  const data = useMemo(() => computeChartValue(statistic), [statistic]);
+
+  const computeChartValue = stats => {
+    const data = [];
+    stats.statistics.forEach(record => {
+      data.push([record.date, parseInt(record.records)]);
+    });
+    return data;
+  };
+
+  return (
+    <Chart
+      className='usage-statistics-chart-content'
+      data={[
+        {
+          label: props.label,
+          data: data.reverse()
+        }
+      ]}
+      axes={[
+        { primary: true, type: 'ordinal', position: 'bottom' },
+        { type: 'linear', position: 'left', scaleIntegersOnly: true }
+      ]}
+      tooltip
+    />
+  );
 }
 
 UsageStatisticsChart.propTypes = {
-	stats: PropTypes.object.isRequired,
-	label: PropTypes.string.isRequired
+  stats: PropTypes.object.isRequired,
+  label: PropTypes.string.isRequired
 };
 
 export default withRouter(UsageStatisticsChart);
