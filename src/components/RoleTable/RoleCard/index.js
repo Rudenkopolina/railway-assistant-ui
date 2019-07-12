341664bdd9707688c057ba98aa64f879ba7f1078
Darya Losich <losich.daria@gmail.com>
2019-07-12 16:46:15 +0300

    roleTable changes

--- src/components/RoleTable/RoleCard/index.js
+++ src/components/RoleTable/RoleCard/index.js
@@ -1,76 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Table } from 'semantic-ui-react'
 import './styles.css';
 
-// const permissions = [
-//   'ALLOWED_USERS_CREATION',
-//   'ALLOWED_KNOWLEDGEBASE_VIEWING',
-//   'ALLOWED_KNOWLEDGEBASE_EDITING',
-//   'ALLOWED_ANSWERS_VIEWING',
-//   'ALLOWED_ANSWERS_EDITING',
-//   'ALLOWED_HISTORY_VIEWING'
-// ];
+
 
 class RoleCard extends React.Component {
-  state = {
-    role: this.props.role,
-  };
+  // state = {
+  //   role: this.props.role,
+  // };
 
-  renderActions = () => {
-    const { permissions } = this.props.role;
-    const isPermissionsChanged = JSON.stringify(this.state.role.permissions.sort()) !== JSON.stringify(permissions.sort());
-    return (
-    <div className='role-table-actions'>
-      <div className="role-table-action no-button">
-      {isPermissionsChanged &&
-        <div
-        className="action-button"
-        onClick={() => this.props.onSave(this.state.role)}
-        >
-          Сохранить
-        </div>
-      }
-      </div>
-    </div>
-    )
-  };
+  // renderActions = () => {
+  //   const { permissions } = this.props.role;
+  //   const isPermissionsChanged = JSON.stringify(this.state.role.permissions.sort()) !== JSON.stringify(permissions.sort());
+  //   return (
+  //   <div className='role-table-actions'>
+  //     <div className="role-table-action no-button">
+  //     {isPermissionsChanged &&
+  //       <div
+  //       className="action-button"
+  //       onClick={() => this.props.onSave(this.state.role)}
+  //       >Сохранить
+  //       </div>
+  //     }
+  //     </div>
+  //   </div>
+  //   )
+  // };
 
   handleCheckbox = value => {
-    let newPermissions = this.state.role.permissions;
-    if (this.state.role.permissions.includes(value)) {
-      newPermissions = newPermissions.filter(item => item !== value);
-    } else {
-      newPermissions = [...newPermissions, value];
-    }
-    this.setState({ role: {...this.state.role, permissions: newPermissions}});
+    const {role} = this.props
+    console.log(value)
+    let newPermissions = role.permissions;
+    console.log(newPermissions);
+    // if (this.state.role.permissions.includes(value)) {
+    //   newPermissions = newPermissions.filter(item => item !== value);
+    // } else {
+    //   newPermissions = [...newPermissions, value];
+    // }
+    // this.setState({ role: {...this.state.role, permissions: newPermissions}});
   };
 
   render() {
-    const { role } = this.props;
+    const { role, permissions } = this.props;
     return (
-        <div className="role-table-row-wrapper">
-          <div className="role-table-row">
-            <div className="role-table-number">{role.id}</div>
-            <div className="role-table-content">
-                {role.type}
-            </div>
-            {role.permissions.map((item, index) =>
-              <div className="role-table-content checkbox-container" key={index}>
-                <input
-                  type="checkbox"
-                  className='role-table-checkbox'
-                  checked={role.permissions.includes(item)}
-                  onChange={() => this.handleCheckbox(item)}
-                />
-              </div>
-            )}
-            {this.renderActions()}
-          </div>
-        </div>
+      <Table.Row>
+        <Table.Cell>{role.type}</Table.Cell>
+        {permissions.map((item, index) =>
+          <Table.Cell key={index}>
+            <input
+              type="checkbox"
+              className='role-table-checkbox'
+              checked={role.permissions.find(p => (p.permission === item.permission))}
+              onChange={() => this.handleCheckbox(item)}
+            /></Table.Cell>)}
+                {/* {this.renderActions()} */}
+      </Table.Row>  
     );
   }
 }
+
 RoleCard.propTypes = {
+  permissions: PropTypes.array.isRequired,
   role: PropTypes.object.isRequired,
   onSave: PropTypes.func.isRequired
 };
--- src/components/RoleTable/index.js
+++ src/components/RoleTable/index.js
@@ -2,39 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import RoleCard from './RoleCard';
+
+import { Table } from 'semantic-ui-react'
 import './styles.css';
 
-// const titles = [
-//   'Роль',
-//   'Просмотр и создание сотрудников',
-//   'Просмотр базы знаний',
-//   'Редактирование базы знаний',
-//   'Просмотр базовых ответов',
-//   'Редактирование базовых ответов',
-//   'Просмотр истории'
-// ]
+const permissionsString = {ALLOWED_EVERYTHING:'Разрешено все', 
+  ALLOWED_ANSWERS_EDITING: 'Редактирование ответов',
+  ALLOWED_KNOWLEDGEBASE_EDITING: 'Редактирование базы знаний',
+  ALLOWED_ANSWERS_VIEWING: 'Просмотр ответов',
+  ALLOWED_KNOWLEDGEBASE_VIEWING: 'Просмотр базы знаний',
+  ALLOWED_ANSWERS_CREATION: 'Создание ответов',
+  ALLOWED_KNOWLEDGEBASE_CREATION: 'Создание базы знаний',
+  ALLOWED_HISTORY_VIEWING: 'Просмотр истории',
+  ALLOWED_HISTORY_EDITING: 'Редактирование истории',
+  ALLOWED_USERS_CREATION: 'Создание пользователей',
+  ALLOWED_USERS_EDITING: 'Редактирование пользователей',
+  ALLOWED_KEYWORDS_VIEWING: 'Просмотр ключевых слов',
+  ALLOWED_USERS_VIEWING: 'Просмотр пользователей',
+  ALLOWED_ROLES_VIEWING: 'Просмотр ролей',
+  ALLOWED_ROLES_EDITING: 'Редактирование ролей',
+  ALLOWED_ROLES_CREATION: 'Создание ролей',
+  ALLOWED_PERMISSION_VIEWING: 'Просмотр разрешений',
+  ALLOWED_USAGE_STATISTICS_VIEWING: 'Просмотр статистики использования',
+  ALLOWED_LOGS_VIEWING: 'Просмотр логов',
+  ALLOWED_CONVERSATION_STATISTICS_VIEWING: 'Просмотр статистики разговоров',}
 
 class RoleTable extends React.Component {
   render() {
     const { roles, onUpdateRole, permissions } = this.props;
     return (
       <div className='role-table-container'>
-        {
-          <div>
-            {permissions.map(item =>
-              <div key={item.id}>
-                {item.permission}
-              </div>
-            )}
-          </div>
-        }
-        {roles.map((item, index) => (
-          <RoleCard
-            key={index}
-            role={item}
-            onSave={onUpdateRole}
-          />
-        ))}
+        <Table basic='very' padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell key={'role'}>Роль</Table.HeaderCell>
+              {permissions.map(item => (
+                <Table.HeaderCell key={item.id}>
+                  {permissionsString[item.permission]}
+                </Table.HeaderCell>
+              ))}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {roles.map((item, index) => (
+              <RoleCard key={index}
+                permissions={permissions}
+                role={item}
+                onSave={onUpdateRole}></RoleCard>))}
+          </Table.Body>
+        </Table>
       </div>
     );
   }
