aff5c649746c359683dc2d723401b793f0d1fdf7
AntonMatveyenka <realanton12345@gmail.com>
2019-07-16 12:28:06 +0300

    Added dependency. Fixed table width. Added conversation type to the table.

--- src/components/ConversationsTable/index.js
+++ src/components/ConversationsTable/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { Table } from 'semantic-ui-react';
+import {Icon, Table} from 'semantic-ui-react';
 
 import './styles.css';
 import moment from 'moment';
@@ -63,7 +63,7 @@ class ConversationsTable extends React.Component {
       return (
         <tfoot>
           <tr>
-            <th colSpan='4'>
+            <th colSpan='5'>
               <div className='block-pagination'>
                 <button
                   className='ui right labeled icon button'
@@ -77,6 +77,15 @@ class ConversationsTable extends React.Component {
           </tr>
         </tfoot>
       );
+    }
+  };
+
+  drawConversationType = (type) => {
+    switch(type) {
+      case "VOICE": return (<div><Icon name='microphone' size='small'/>Голос</div>);
+      case "TEXT": return (<div><Icon name='align justify' size='small'/>Текст</div>);;
+      case "MIXED": return (<div><Icon name='sync' size='small'/>Смешанный</div>);;
+      case "UNKN": return (<div><Icon name='question circle' size='small'/>Неизвестен</div>);;
     }
   };
 
@@ -117,6 +126,7 @@ class ConversationsTable extends React.Component {
               >
                 Количество шагов
               </Table.HeaderCell>
+              <Table.HeaderCell textAlign='center'>Тип</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
@@ -141,6 +151,9 @@ class ConversationsTable extends React.Component {
                 </Table.Cell>
                 <Table.Cell textAlign='center'>
                   {conversation.iterations}
+                </Table.Cell>
+                <Table.Cell textAlign='center'>
+                  {this.drawConversationType(conversation.type)}
                 </Table.Cell>
               </Table.Row>
             ))}
